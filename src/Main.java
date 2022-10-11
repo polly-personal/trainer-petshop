bb3d02db5a6dbd72fcdea2f051da34fa84c29708
Polly <polly.personal.01@yandex.ru>
2022-10-11 13:54:33 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        OrdersManager ordersManager = new OrdersManager();
+
+        ordersManager.printAllOrders();
+        System.out.println("Всего заказов на сумму: " + ordersManager.getOrdersSum());
+
+        String maxOrderCustomerNqame = ordersManager.getMaxOrderCustomerName();
+        System.out.println("Самая большая сумма заказов у " + maxOrderCustomerName);
+        ordersManager.printCustomerOrders(maxOrderCustomerName);
+
+        ordersManager.removeUnprofitableOrders();    }
+}
